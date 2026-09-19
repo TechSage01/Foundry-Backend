d452b3965f9b3904cd3ab3b6faeb7c6a204cad2a
Habeebamoo <habeebamoo08@gmail.com>
2026-09-19 18:06:38 +0100

    added PATCH to CORS middleware

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   cors({
     origin: [process.env.CLIENT_URL!],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"]
   })(req, res, next)
 })
